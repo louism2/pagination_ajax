bcd9b861a2c3792a43cbe6aad1d6c99b174e154b
louism2 <louiscmancini@gmail.com>
2014-06-08 16:31:54 -0700

    moved plugin to seperate file

--- lib/generators/pagination_ajax/install/install_generator.rb
+++ lib/generators/pagination_ajax/install/install_generator.rb
@@ -6,12 +6,12 @@ module PaginationAjax
         # for JavaScript application.js manifest:        
         if File.exists? "#{Rails.root}/app/assets/javascripts/application.js"
           append_to_file "app/assets/javascripts/application.js" do
-            "//= require pagination_ajax\n"
+            "\n//= require pagination_ajax_plugin\n//= require pagination_ajax\n"
           end    
         # ...or for CoffeeScript application.js.coffee manifest:
         elsif File.exists? "#{Rails.root}/app/assets/javascripts/application.js.coffee"   
           append_to_file "app/assets/javascripts/application.js.coffee" do
-            "\n#= require pagination_ajax\n"
+            "\n#= require pagination_ajax_plugin\n#= require pagination_ajax\n"
           end
         end
       end
--- vendor/assets/javascripts/pagination_ajax.js
+++ vendor/assets/javascripts/pagination_ajax.js
@@ -94,31 +94,6 @@ var ajaxPagination = {
 	
 } // end object	
 
-(function ($) {
-  $.fn.handleLinkClick = function(){
-  	
-	$(this).on('click', function(e){
-		e.preventDefault();
-		var $el = $(this);
-		var $parent = $el.closest('.pagination_container');
-		var $p_canv = $el.parent().siblings('.pagination_canvas');
-		var data_attributes = $parent.data();	
-		var server_response = ajaxPagination.sendPaginationRequest(data_attributes);
-		var response;
-		server_response.done(function(data){
-			response = {status: 'success', data: data};
-			ajaxPagination.buildDomElements($p_canv, data_attributes.template, response);
-			ajaxPagination.drawLinkIcons($el, $parent, response);
-		}).fail(function(jqXHR, textStatus, errorThrown){
-			response = {status: 'fail', data: []};
-			ajaxPagination.buildDomElements($p_canv, data_attributes.template, response);
-		});
-	});
-	
-  };
-})(jQuery);
-
-	
 $(document).ready(function(){
 	
 	if($('.selected_page').length > 0){
